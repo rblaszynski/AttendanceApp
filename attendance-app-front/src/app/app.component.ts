37df81299107f5239a83d50c170824f91325720e
Robert Błaszyński <robert.blaszynski@student.put.poznan.pl>
2019-04-07 17:08:49 +0200

    Dark mode fix

--- attendance-app-front/src/app/app.component.ts
+++ attendance-app-front/src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {UiService} from "./services/ui/ui.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less']
 })
-export class AppComponent {
-  title = 'attendance-app-front';
+export class AppComponent implements OnInit{
+  showMenu = false;
+  darkModeActive: boolean;
+
+  constructor(public ui: UiService) {
+
+  }
+
+  ngOnInit() {
+    this.ui.darkModeState.subscribe((value => {
+      this.darkModeActive = value;
+    }))
+  }
+
+  toggleMenu() {
+    this.showMenu = !this.showMenu;
+  }
+
+  modeToggleSwitch() {
+    this.ui.darkModeState.next(!this.darkModeActive);
+  }
 }
--- attendance-app-front/src/app/app.module.ts
+++ attendance-app-front/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {UiService} from "./services/ui/ui.service";
 
 @NgModule({
   declarations: [
@@ -12,7 +13,9 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    UiService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
