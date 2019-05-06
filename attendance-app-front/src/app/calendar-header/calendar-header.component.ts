416551ea78baa9819fa338b267156c6dc9bf9813
Robert Błaszyński <robert.blaszynski@student.put.poznan.pl>
2019-05-06 13:42:36 +0200

    calendar header dark mode fix

--- attendance-app-front/src/app/calendar-header/calendar-header.component.ts
+++ attendance-app-front/src/app/calendar-header/calendar-header.component.ts
@@ -1,11 +1,21 @@
-import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {UiService} from "../services/ui/ui.service";
 
 @Component({
   selector: 'mwl-demo-utils-calendar-header',
   templateUrl: './calendar-header.component.html',
   styleUrls: ['./calendar-header.component.less']
 })
-export class CalendarHeaderComponent {
+export class CalendarHeaderComponent implements OnInit {
+  darkModeActive: boolean;
+  constructor(public ui: UiService) {}
+
+  ngOnInit() {
+    this.ui.darkModeState.subscribe((value => {
+      this.darkModeActive = value;
+    }))
+  }
+
   @Input() view: string;
 
   @Input() viewDate: Date;
