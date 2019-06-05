16bc6c42b94fc407ab74acdeffe2d8c2e2718d90
Michał Andrzejewski <36901567+michal-andrzejewski@users.noreply.github.com>
2019-06-05 14:40:00 +0200

    Update db_connection.py
    query to json

--- attendance-app-api/db_connection.py
+++ attendance-app-api/db_connection.py
@@ -1,4 +1,5 @@
 import pyodbc
+import json
 # Some other example server values are
 # server = 'localhost\sqlexpress' # for a named instance
 # server = 'myserver,port' # to specify an alternate port
@@ -10,5 +11,24 @@ cnxn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';
 cursor = cnxn.cursor()
 
 cursor.execute("select * from Obecnosci;")
-for row in cursor:
-    print(row)
+data = []
+rows = cursor.fetchall()
+for row in rows:
+    data.append([x for x in row])
+
+print(data)
+
+
+def query_db(query, args=(), one=False):
+    cur = cnxn.cursor()
+    cur.execute(query, args)
+    r = [dict((cur.description[i][0], value) \
+               for i, value in enumerate(row)) for row in cur.fetchall()]
+    cur.connection.close()
+    return (r[0] if r else None) if one else r
+
+
+my_query = query_db("select * from Obecnosci;", )
+
+json_output = json.dumps(my_query)
+print(json_output)
