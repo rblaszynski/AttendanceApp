05767aa78b07954d85123816a3f5083fe8fa6bff
Dominik Błaszczyk <37297577+DominikBlaszczyk@users.noreply.github.com>
2019-06-05 11:34:43 +0200

    Modified table

---
+++ attendance-app-database/Modified.sql
@@ -0,0 +1,23 @@
+
+ALTER TABLE Obecnosci 
+DROP COLUMN "isPresent"
+
+ALTER TABLE Obecnosci
+ADD "grupa" varchar(4)
+
+ALTER TABLE Obecnosci
+ADD "startZajec" varchar(5)
+
+ALTER TABLE Obecnosci
+ADD "koniecZajec" varchar(5)
+
+ALTER TABLE Obecnosci 
+ADD "isPresent" bit
+
+
+INSERT INTO Obecnosci(id,data,nr_legitymacji,przedmiot,grupa, startZajec, koniecZajec,isPresent)
+values(1,'2019-06-05', '013697D7','Podstawy teleinformatyki','TI-1', '15:10','16:40',0)
+INSERT INTO Obecnosci(id,data,nr_legitymacji,przedmiot,grupa, startZajec, koniecZajec,isPresent)
+values(2,'2019-06-05', '0136C21C','Podstawy teleinformatyki','TI-1', '15:10','16:40',0)
+
+select * from Obecnosci
